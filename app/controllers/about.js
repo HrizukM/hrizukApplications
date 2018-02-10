bbbc73660a43157f3679f398596eae3eac78d708
EC2 Default User <ec2-user@ip-172-31-40-95.ec2.internal>
2018-02-10 05:48:18 +0000

    Working on feature on about page

--- app/controllers/about.js
+++ app/controllers/about.js
@@ -1,5 +1,9 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
-    text:'test text to see if I am using controllers correctly',
+    actions:{
+        toggleBody(){
+            this.toggleProperty('isExpanded');
+        }
+    }
 });
