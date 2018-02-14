e8e18af72f93e06c0f4724e9e225dcd6c6201402
Michael Hrizuk <mhrizuk@michaels-mbp.lan>
2018-02-13 22:38:48 -0600

    Thinking about changing how this information is displayed

--- app/controllers/about.js
+++ app/controllers/about.js
@@ -6,10 +6,10 @@ export default Controller.extend({
             this.toggleProperty('isExpanded');
         },
         toggleP2(){
-            this.toggleProperty('isExpanded2')
+            this.toggleProperty('isExpanded2');
         },
         toggleP3(){
-            this.toggleProperty('isExpanded3')
+            this.toggleProperty('isExpanded3');
         }
     },
     education:"Placing assured be if removed it besides on. Far shed each high read are men over day. Afraid we praise lively he suffer family estate is. Ample order up in of in ready. Timed blind had now those ought set often which. Or snug dull he show more true wish. No at many deny away miss evil. On in so indeed spirit an mother. Amounted old strictly but marianne admitted. People former is remove remain as.",
